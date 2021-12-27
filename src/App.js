4d04eef4eb0e623d1e8f941ea2e6af320cff1c87
Seeya0 <vinlandtorfin@gmail.com>
2021-12-27 21:00:15 +0900

    フロントとバックエンドの準備

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import Login from "./components/Login";
+import Home from "./container/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
 };
 
