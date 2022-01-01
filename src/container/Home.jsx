bf80aedbf0bc261e84db3900d631fa1f05e00370
Seeya0 <vinlandtorfin@gmail.com>
2022-01-01 22:46:25 +0900

    Home画面作成

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+import { Sidebar, UserProfile, Login } from "../components";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { client } from "../client";
+import { userQuery } from "../utils/data";
 
 const Home = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const userInfo =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : localStorage.clear();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, []);
+
   return (
-    <div>
-      <h1>Welcom to the Jungle!!</h1>
+    <div className="flex md:flex-row bg-gray-50 flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar user={user && user} />
+      </div>
+      <div className="flex flex-row md:hidden">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(true)}
+        />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="profileimage" className="w-9 h-9" />
+        </Link>
+      </div>
+      {toggleSidebar && (
+        <div className="flex w-4/5 bg-white h-screen overflow-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle
+              fontSize={30}
+              className="cursor-pointer"
+              onClick={() => setToggleSidebar(false)}
+            />
+          </div>
+          <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+        </div>
+      )}
     </div>
   );
 };
