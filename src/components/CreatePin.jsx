16435471e90119113600e24bd9894c1996eeb697
Seeya0 <vinlandtorfin@gmail.com>
2022-01-03 21:38:18 +0900

    Navbarを追加

---
+++ src/components/CreatePin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatePin = () => {
+  return (
+    <div>
+      <h1>Create Pin!</h1>
+    </div>
+  );
+};
+
+export default CreatePin;
---
+++ src/components/PinDetail.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PinDetail = () => {
+  return (
+    <div>
+      <h1>Pimdetail</h1>
+    </div>
+  );
+};
+
+export default PinDetail;
