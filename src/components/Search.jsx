240f12079739a808bb48d3b48dfa6134e374f20c
yoshioka.seiya <yoshioka.seiya@hc-sys.jp>
2022-01-04 20:45:19 +0900

    SearchとFeedを追加

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { feedQuery, searchQuery } from "../utils/data";
+import { client } from '../client'
+import Spinner from './Spinner'
+import MasonryLayout from "./MasonryLayout";
 
-const Search = () => {
+const Search = ({searchTerm}) => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
   return (
-    <div>
-      <h1>Search!!</h1>
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      {loading && <Spinner message="Searching pins" />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl">No Pins Found!</div>
+      )}
     </div>
   );
 };
